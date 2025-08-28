3d87469a553308b36727d75755c9b39d3fc3cc6f
hamed <nlitny@gmail.com>
2025-08-28 11:58:36 +0330

    local postgress config

--- backend/db/init.js
+++ backend/db/init.js
@@ -1,47 +1,54 @@
 const { Client } = require("pg");
 const { execSync } = require("child_process");
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const path = require("path");
+
+// Load .env
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+
 async function initDB() {
   const client = new Client({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "postgres",
-    password: "sadafpj6761",
-    port: 5432,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
   });
 
   try {
     await client.connect();
 
+    // CREATE ROLE
     await client.query(`
       DO $$
       BEGIN
-        IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'ordertracking') THEN
-          CREATE ROLE ordertracking LOGIN PASSWORD 'OrderTrack2024';
+        IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${process.env.ORDERTRACKING_USER}') THEN
+          CREATE ROLE ${process.env.ORDERTRACKING_USER} LOGIN ENCRYPTED PASSWORD '${process.env.ORDERTRACKING_PASSWORD}';
         END IF;
       END
       $$;
     `);
 
+    // CREATE DATABASE
     const res = await client.query(
-      "SELECT 1 FROM pg_database WHERE datname = 'order_tracking_db'"
+      `SELECT 1 FROM pg_database WHERE datname = '${process.env.ORDERTRACKING_DB}'`
     );
     if (res.rowCount === 0) {
       await client.query(
-        "CREATE DATABASE order_tracking_db OWNER ordertracking"
+        `CREATE DATABASE ${process.env.ORDERTRACKING_DB} OWNER ${process.env.ORDERTRACKING_USER}`
       );
       console.log("Database created");
     } else {
       console.log("Database already exists");
     }
 
+    // GRANT PRIVILEGES
     await client.query(
-      "GRANT ALL PRIVILEGES ON DATABASE order_tracking_db TO ordertracking"
+      `GRANT ALL PRIVILEGES ON DATABASE ${process.env.ORDERTRACKING_DB} TO ${process.env.ORDERTRACKING_USER}`
     );
 
     console.log("Database initialized");
 
+    // Prisma migrations
     console.log("Running Prisma migrations & seed...");
     execSync("npx prisma generate", { stdio: "inherit" });
     execSync("npx prisma migrate dev --name init", { stdio: "inherit" });
